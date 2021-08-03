bc5f926f6e49237ae2a0d4625437c906abbe1262
Jacob Errington <jerrington@drw.com>
2021-08-03 00:08:20 -0400

    rework types

--- types.ts
+++ types.ts
@@ -1,43 +1,78 @@
-##--```json
-##{
-#  Monday :
-#    [
-#       {
-#          name : chicken,
-#          ingredients : [...]
-#        },
-#        { ... },
-#        { ... },
-#    ],
-#  Tuesday :
-#    [
-#      {
-#```
+type MealTime = "breakfast" | "lunch" | "dinner";
 
-interface MealTicket {
-    anonymous: boolean;
-    breakfasty: boolean;
-    name: string;
+type MealType = "anonymous" | "named";
+
+interface BasicMeal {
+  /**
+   * The times that this meal could be eaten at.
+   */
+  times: MealTime[];
 }
 
-type breakfastability = "exculsively" | "possibly" | "never";
-type mealTime = "Breakfast" | "Lunch" | "Dinner";
+interface NamedMeal extends BasicMeal {
+  type: "named";
+  name: string;
+  recipe: Recipe;
+}
 
 interface Recipe {
-    name: string;
-    ingredients: string[];
-    instrutions: string[];
-    breakfastable: breakfastability;
+  ingredients: string[];
+  instrutions: string[];
 }
 
-interface RecipesByTiming {
-    recipes: Recipe[];
-    mealsTime: mealTime;
+interface AnonymousMeal extends BasicMeal {
+  type: "anonymous";
+  ingredients: string[];
 }
 
-interface RecipesByTiming{
-    dailyRecipes: Recipes[];
-}
+type Meal = NamedMeal | AnonymousMeal;
 
 
+/**
+ * The array always has exactly 7 elements. The first is understood as Sunday's meals.
+ */
+type WeeklyMealPlan = Array<{
+  breakfast: Meal;
+  lunch: Meal;
+  dinner: Meal;
+}>;
 
+/* IDEA:
+ * - we store objects of type Meal in the DB
+ * - planner inputs every Meal from the DB and outputs 1 WeeklyMealPlan
+ *
+ * Notice that the Meal has a _list_ of potential times it could be eaten at, but that's OK in the plan,
+ * since the position of the Meal in the plan tells us when we're going to be eating it this week.
+ */
+
+// Example:
+const chickenDinner: AnonymousMeal = {
+  type: "anonymous",
+  ingredients: ["chicken", "rice", "broccoli"],
+  times: ["dinner"],
+};
+const flankSteakDinner: AnonymousMeal = {
+  type: "anonymous",
+  ingredients: ["flank steak", "potatoes", "asparagus"],
+  times: ["dinner"],
+};
+const pancakes: NamedMeal = {
+  type: "named",
+  name: "pancakes",
+  times: ["breakfast"],
+};
+const grilledCheeseSandwich: NamedMeal = {
+  type: "named",
+  name: "grilled cheese sandwich",
+  times: ["snack", "lunch"]
+};
+const cereal: NamedMeal = {
+  type: "named",
+  name: "cereal",
+  times: ["breakfast"],
+};
+
+const plan: WeeklyMealPlan = [
+  { breakfast: cereal, lunch: grilledCheeseSandwich, dinner: chickenDinner },
+  // ... (6 more days)
+];
